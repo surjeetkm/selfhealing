e6d3dd6b235e733ea79dc09ecc9a87638a8c8ebd
Surjeet Mohanty <surjeetmohanty2019@gmail.com>
2020-05-02 16:48:54 +0530

    addes

--- src/main/java/com/c2p/selfhealing/SelfHealingTestApplication.java
+++ src/main/java/com/c2p/selfhealing/SelfHealingTestApplication.java
@@ -3,14 +3,21 @@ package com.c2p.selfhealing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SelfHealingTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SelfHealingTestApplication.class, args);
 	}
 
+	@RequestMapping("/get")
+	public String getHello() {
+		return "Hello!";
+	}
 	@Bean
 	public CustomHealthIndicator getIndicator() {
 		return new CustomHealthIndicator();
