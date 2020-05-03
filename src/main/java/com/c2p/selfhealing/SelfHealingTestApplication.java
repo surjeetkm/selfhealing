60172949a243e49e41e1d10f800228d3b712ff7e
Surjeet Mohanty <surjeetmohanty2019@gmail.com>
2020-05-03 15:56:23 +0530

    Config map test

--- src/main/java/com/c2p/selfhealing/SelfHealingTestApplication.java
+++ src/main/java/com/c2p/selfhealing/SelfHealingTestApplication.java
@@ -1,5 +1,6 @@
 package com.c2p.selfhealing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +14,11 @@ public class SelfHealingTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SelfHealingTestApplication.class, args);
 	}
-
+	@Value("${message:Welcome Default message}")
+	private String message;
 	@RequestMapping("/get")
 	public String getHello() {
-		return "Hello Wecome to Self Heal!";
+		return "Hello Wecome to Self Heal!"+ ":"+message;
 	}
 	@Bean
 	public CustomHealthIndicator getIndicator() {
